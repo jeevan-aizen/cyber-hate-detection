c51e67fa451b8e2f63aec38c28e017ff0926ebac
jeevan-aizen <jeevanchowdary604@gmail.com>
2024-08-30 17:52:48 +0530

    Add files via upload

---
+++ a_multi_stage_machine_learning/Service_Provider/views.py
@@ -0,0 +1,214 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
+from django.db.models import Count
+from django.db.models import Q
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Create your views here.
+from Remote_User.models import ClientRegister_Model,cyber_hate_detection,detection_ratio,detection_accuracy
+
+
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
+        password = request.POST.get('password')
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
+
+    return render(request,'SProvider/serviceproviderlogin.html')
+
+def View_Predicted_Cyber_Hate_Analysis_Type_Ratio(request):
+    detection_ratio.objects.all().delete()
+    ratio = ""
+    kword = 'Cyber Hate'
+    print(kword)
+    obj = cyber_hate_detection.objects.all().filter(Q(Prediction=kword))
+    obj1 = cyber_hate_detection.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
+
+    ratio1 = ""
+    kword1 = 'Cyberbullying'
+    print(kword1)
+    obj1 = cyber_hate_detection.objects.all().filter(Q(Prediction=kword1))
+    obj11 = cyber_hate_detection.objects.all()
+    count1 = obj1.count();
+    count11 = obj11.count();
+    ratio1 = (count1 / count11) * 100
+    if ratio1 != 0:
+        detection_ratio.objects.create(names=kword1, ratio=ratio1)
+
+    ratio12 = ""
+    kword12 = 'Neutral'
+    print(kword12)
+    obj12 = cyber_hate_detection.objects.all().filter(Q(Prediction=kword12))
+    obj112 = cyber_hate_detection.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
+
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Predicted_Cyber_Hate_Analysis_Type_Ratio.html', {'objs': obj})
+
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
+
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
+
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
+
+def View_Predicted_Cyber_Hate_Analysis_Type(request):
+    obj =cyber_hate_detection.objects.all()
+    return render(request, 'SProvider/View_Predicted_Cyber_Hate_Analysis_Type.html', {'list_objects': obj})
+
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
+
+
+def Download_Predicted_DataSets(request):
+
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Datasets.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = cyber_hate_detection.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+
+        row_num = row_num + 1
+
+        ws.write(row_num, 0, my_row.Fid, font_style)
+        ws.write(row_num, 1, my_row.tweet, font_style)
+        ws.write(row_num, 2, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+
+    df = pd.read_csv('Datasets.csv')
+
+    def apply_response(Label):
+        if (Label == 0):
+            return 0
+        elif (Label == 1):
+            return 1
+        elif (Label == 2):
+            return 2
+
+    df['results'] = df['Label'].apply(apply_response)
+
+    cv = CountVectorizer()
+    X = df['tweet']
+    y = df['results']
+
+    print("Tweet")
+    print(X)
+    print("Results")
+    print(y)
+
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    print("Naive Bayes")
+
+    from sklearn.naive_bayes import MultinomialNB
+
+    NB = MultinomialNB()
+    NB.fit(X_train, y_train)
+    predict_nb = NB.predict(X_test)
+    naivebayes = accuracy_score(y_test, predict_nb) * 100
+    print("ACCURACY")
+    print(naivebayes)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_nb))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_nb))
+    detection_accuracy.objects.create(names="Naive Bayes", ratio=naivebayes)
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    models.append(('svm', lin_clf))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    models.append(('logistic', reg))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    print("Decision Tree Classifier")
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    dtcpredict = dtc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, dtcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, dtcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, dtcpredict))
+    models.append(('DecisionTreeClassifier', dtc))
+    detection_accuracy.objects.create(names="Decision Tree Classifier", ratio=accuracy_score(y_test, dtcpredict) * 100)
+
+
+    csv_format = 'Results.csv'
+    df.to_csv(csv_format, index=False)
+    df.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
---
+++ pythonsoftwaresmain/package2.bat
@@ -0,0 +1,6 @@
+pip install pandas sklearn matplotlib seaborn
+pip install numpy==1.18
+pip install scipy==1.1.0
+pip install scikit-learn==0.21.3
+pip install keras==2.2.4
+pip install tensorflow==1.14.0
